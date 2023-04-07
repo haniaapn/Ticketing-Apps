ff8831fb162360161f45d224b69975c871d7e3a3
haohann <haniapn@webmail.umm.ac.id>
2023-04-07 14:25:05 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/ticketingapps/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.dicoding.ticketingapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
+
+        val seatsView = findViewById<SeatsView>(R.id.seatsView)
+        val button = findViewById<Button>(R.id.finishButton)
+        button.setOnClickListener {
+            seatsView.seat?.let{
+                Toast.makeText(this, "Kursi anda nomer ${it.name}.", Toast.LENGTH_SHORT).show()
+            } ?: run{
+                Toast.makeText(this, "Silahkan pilih kursi terlebih dahulu", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
